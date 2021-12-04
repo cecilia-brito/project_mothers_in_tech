d540bb5c5ef74d7fbec13c9a3d6f6130d3a9f9c3
Cecilia Brito Santos <ceciliabritosantos@gmail.com>
2021-12-03 21:10:48 -0300

    update

---
+++ scripts/filters.js
@@ -0,0 +1,20 @@
+const job1 = document.getElementById('1');
+const job2 = document.getElementById('2');
+const job3 = document.getElementById('3');
+const job4 = document.getElementById('4');
+const job5 = document.getElementById('5');
+const checkvagas = document.getElementById('check-vagas');
+const checkcursos = document.getElementById('check-cursos');
+const listFilters = document.getElementById('filters');
+
+function filtrando(){
+    if(checkvagas.checked){
+        job2.classList.toggle('active');
+        job3.classList.toggle('active');
+        job1.classList.toggle('active');
+        job4.classList.toggle('desactive');
+        job2.classList.toggle('desactive{');
+        job5.classList.toggle('desactive{');
+    }
+}
+
